c9109b05462a9310440303ba57b3e002f8e39ca1
bikem <bengisuozaydin@gmail.com>
2022-01-10 23:54:13 +0300

    CURRENTLY NOT WORKING

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const contract = require('./contract.js')
 const express = require('express')
 const app = express()
 const port = 3000
+
+verificationcontract = contract.verificationcontract;
+productprovenancecontract = contract.productprovenancecontract;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
