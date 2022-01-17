8948786e62259b0e4b9e7f7e42507bda84795642
dogukanakar <dogukanakarc@gmail.com>
2022-01-17 03:32:45 +0300

    created basic routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import QueryVerified from "./Verification/QueryVerified";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+        <Routes>
+            <Route path={"/queryverified"} element={<QueryVerified/>}/>
+            <Route path={"*"} element={<QueryVerified/>}/>
+        </Routes>
+
+      </React.Fragment>
   );
 }
 
