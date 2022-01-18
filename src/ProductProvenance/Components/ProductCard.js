f8efc5cef3f4705d2bd365676cd941e24d80e7ca
dogukanakar <dogukanakarc@gmail.com>
2022-01-18 04:39:30 +0300

    removed navigation from product card

--- src/ProductProvenance/Components/ProductCard.js
+++ src/ProductProvenance/Components/ProductCard.js
@@ -2,8 +2,6 @@ import React, {useEffect, useState} from 'react'
 import CardContent from "@mui/material/CardContent";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import {useNavigate} from "react-router-dom";
-import ButtonBase from "@mui/material/ButtonBase";
 import Skeleton from "@mui/material/Skeleton";
 import Avatar from "@mui/material/Avatar";
 import PersonIcon from '@mui/icons-material/Person';
@@ -17,7 +15,6 @@ const productProvenanceInterface = new utils.Interface(productProvenanceAbi)
 export default function ProductCard(props){
     const [user, setUser] = useState({})
     const [loading, setLoading] = useState(true)
-    const navigate = useNavigate()
     const [serial, zipCode] =
     useContractCall({
             abi: productProvenanceInterface,
@@ -41,22 +38,14 @@ export default function ProductCard(props){
         </Card>
     ):(
         <Card sx={{ marginTop:1 }}>
-            <ButtonBase
-                style={{
-                    display: 'block',
-                    textAlign: 'initial',
-                    width: "100%"
-                }}
-                onClick={_=>navigate("/profile/"+user.username)}>
-                <CardHeader
-                    avatar={
-                        <Avatar >
-                            <PersonIcon/>
-                        </Avatar>
-                    }
-                    title={"Serial No:" + user.serial + " , <br/>Factory Zip Code" + user.zipCode }
-                />
-            </ButtonBase>
+            <CardHeader
+                avatar={
+                    <Avatar >
+                        <PersonIcon/>
+                    </Avatar>
+                }
+                title={"Serial No:" + user.serial + " , <br/>Factory Zip Code" + user.zipCode }
+            />
         </Card>
     )
 }
