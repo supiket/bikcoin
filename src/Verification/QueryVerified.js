e20f81049469ea97995d3ca1b3069bd887cf67fd
dogukanakar <dogukanakarc@gmail.com>
2022-02-04 15:22:14 +0300

    added addressQuery field to queryverified

--- src/Verification/QueryVerified.js
+++ src/Verification/QueryVerified.js
@@ -15,33 +15,25 @@ const verificationInterface = new utils.Interface(verificationAbi)
 
 export default function QueryVerified() {
     const [address, setAddress] = useState("")
-    const [query, setQuery] = useState(false)
-    const [queried, setQueried] = useState(false)
-    const [verified, setVerified] = useState(false)
+    const [addressQuery, setAddressQuery] = useState("")
     const [isVerified] = // Makes a contract call using abi interface and verification address of the verification contract and shows thte result
         useContractCall({
                 abi: verificationInterface,
                 address: verificationAddress,
                 method: "queryVerified", 
-                args: [address],
+                args: [addressQuery],
             }
         ) ?? [];
-
-    if(query || (isVerified && !verified)){
-        setQueried(true)
-        setVerified(isVerified || false)
-        setQuery(false)
-    }
     return (
         <div>
             <Typography variant="h3" component="div" gutterBottom align={"center"}>
                 Query Verified
             </Typography>
             <Typography variant="body1" component="div" gutterBottom align={"center"}>
-                {(queried)&&
-                    (`The address ${address} is ${verified?"":"not "}verified.`)
+                {addressQuery!=="" && isVerified!==undefined &&
+                    (`The address ${addressQuery} is ${isVerified?"":"not "}verified.`)
                 }
             </Typography>
-            <TextField style={{marginBottom: 15}} fullWidth label="Address" value={address} onChange={e=>setAddress(e.target.value)||setQueried(false)}/>
-            <Button variant="contained" fullWidth onClick={_=>setQuery(true)}>Query</Button></div>);
+            <TextField style={{marginBottom: 15}} fullWidth label="Address" value={address} onChange={e=>setAddress(e.target.value)}/>
+            <Button variant="contained" fullWidth onClick={_=>setAddressQuery(address)}>Query</Button></div>);
 }
